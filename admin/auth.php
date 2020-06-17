956c48332625a444ba26691e3d8e645c49e4e4ee
Wijayaac <wijayaac25@gmail.com>
2020-06-17 10:50:40 +0800

    mengganti semua script dir karena error saat hosting

--- admin/auth.php
+++ admin/auth.php
@@ -8,7 +8,7 @@ Website: http://www.allphptricks.com/
 <?php
 session_start();
 if (!isset($_SESSION["username"])) {
-    // menggantikan header
+    // menggantikan header location karena di hosting terjadi error
     echo '<meta http-equiv="refresh" content="1;url=login.php">';
     exit();}
 ?>
